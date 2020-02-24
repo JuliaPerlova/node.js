036409f532fe54f4eac87c580a2526917f20e9b3
Julia Perlova <dobrpsih@gmail.com>
2020-02-24 21:38:55 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import _ from 'lodash';
-import standart from './slice.js';
-const array = new standart([1, 2, 3, 4, 5, 'a', 'b', 'c', 'd']);
+import Slice from './slice.js';
+const array = new Slice([1, 2, 3, 4, 5, 'a', 'b', 'c', 'd']);
 console.log(_.chunk(array.newSlice(2, 10), 2));
