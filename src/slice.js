1d643fe582b05056bd7b424a7c59e70be612deae
Julia Perlova <dobrpsih@gmail.com>
2020-02-24 21:37:25 +0200

    Update slice.js

--- src/slice.js
+++ src/slice.js
@@ -2,9 +2,10 @@ export default class {
   constructor(arr) {
     this.arr = arr;
   }
+
   newSlice(num1 = 0, num2 = this.arr.length) {
     const oldArr = this.arr;
-    const newArr = oldArr.filter(el => oldArr.indexOf(el) >= num1 && oldArr.indexOf(el) < num2);
+    const newArr = oldArr.filter((el, i) => i >= num1 && i < num2);
     return newArr;
   }
 };
