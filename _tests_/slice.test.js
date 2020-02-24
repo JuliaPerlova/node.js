3b91f16158065ccf4c6fc99da7a4190e1e541e70
Julia Perlova <dobrpsih@gmail.com>
2020-02-24 21:35:46 +0200

    Update slice.test.js

--- _tests_/slice.test.js
+++ _tests_/slice.test.js
@@ -1,13 +1,13 @@
-import slice from '../src/slice.js';
+import Slice from '../src/slice.js';
 
-const array = new slice([1, 3, 7, 8, 9]);
+const array = new Slice([1, 3, 7, 8, 9]);
 test('slice', () => {
   expect(array.newSlice(2, 4)).toStrictEqual([7, 8]);
   expect(array.newSlice(1, 10)).toStrictEqual([3, 7, 8, 9]);
   expect(array.newSlice(2)).toStrictEqual([7, 8, 9]);
 });
 test('rare cases', () => {
-  const empty = new slice([]);
+  const empty = new Slice([]);
   expect(empty.newSlice(3, 94)).toStrictEqual([]);
   expect(array.newSlice()).toStrictEqual([1, 3, 7, 8, 9]);
 });
